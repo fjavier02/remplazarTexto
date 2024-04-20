aeb5e76d853cbe4ce4d4ea4209d86a4941d3a21a
Francisco Lopez <wkm.jtirado@terceiros.mag.com.br>
2024-04-20 06:15:18 -0300

    ajustando textoutput boton de copiar y estilos

---
+++ scritp.js
@@ -0,0 +1,34 @@
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var inputText = document.getElementById("inputTextArea").value;
+  var outputText = inputText
+    .replace(/[\u202F]/g, " ")
+    .replace(/[\u2013]/g, "-")
+    .replace(/[^\d\w\s[À-ü].,]/g, "");
+  document.getElementById("outputTextArea").value = outputText;
+
+  document
+    .getElementById("inputTextArea")
+    .addEventListener("focus", function () {
+      document.getElementById("outputTextArea").value = "";
+    });
+});
+
+document.getElementById("copyButton").addEventListener("click", function () {
+  event.preventDefault();
+
+  var outputTextArea = document.getElementById("outputTextArea");
+  outputTextArea.select();
+  document.execCommand("copy");
+
+  var copyButton = document.getElementById("copyButton");
+  copyButton.classList.add("clicked");
+
+  var copiedText = document.getElementById("copiedText");
+  copiedText.style.opacity = 1;
+
+  setTimeout(function () {
+    copiedText.style.opacity = 0;
+  }, 2000);
+});
